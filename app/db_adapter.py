d351d0b6aa0308347fe295502291531fba4e0e63
Chibuzor Nwachukwu <58276420+chibuzordev@users.noreply.github.com>
2025-10-24 10:01:03 +0100

    Refactor db_adapter to use environment variables

--- app/db_adapter.py
+++ app/db_adapter.py
@@ -3,7 +3,7 @@ import json, os
 from typing import List, Dict
 from .models import Property
 
-DATA_PATH = "../data.json"
+DATA_PATH = os.getenv("DATA_PATH", "data.json")
 ANALYSIS_CACHE = os.getenv("ANALYSIS_CACHE", "analysis_cache.json")
 
 class LocalDBAdapter:
@@ -103,6 +103,7 @@ class DBAdapter:
         and update DB accordingly (JSONB column).
         """
         raise NotImplementedError("Implement update_analysis_batch to persist analysis into DB")
+
 
 
 
